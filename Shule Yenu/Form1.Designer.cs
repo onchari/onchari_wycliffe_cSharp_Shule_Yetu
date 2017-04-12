b13c5ab83602190939af2e053eabd48ee10bca13
onchari wycliffe <oncha@DESKTOP-06IKRA6>
2017-04-12 14:54:10 +0300

    first commit of my project to Github

---
+++ Shule Yenu/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Shule_Yenu
+{
+    partial class Login_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login_Form));
+            this.Login_area = new System.Windows.Forms.GroupBox();
+            this.button_cancel = new System.Windows.Forms.Button();
+            this.button_login = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox_level = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.Login_area.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Login_area
+            // 
+            this.Login_area.BackColor = System.Drawing.Color.White;
+            this.Login_area.Controls.Add(this.button_cancel);
+            this.Login_area.Controls.Add(this.button_login);
+            this.Login_area.Controls.Add(this.label6);
+            this.Login_area.Controls.Add(this.label5);
+            this.Login_area.Controls.Add(this.comboBox_level);
+            this.Login_area.Controls.Add(this.label3);
+            this.Login_area.Controls.Add(this.textBox_password);
+            this.Login_area.Controls.Add(this.label4);
+            this.Login_area.Controls.Add(this.label2);
+            this.Login_area.Controls.Add(this.textBox_username);
+            this.Login_area.Controls.Add(this.label1);
+            this.Login_area.Controls.Add(this.pictureBox1);
+            this.Login_area.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login_area.Location = new System.Drawing.Point(43, 12);
+            this.Login_area.Name = "Login_area";
+            this.Login_area.Size = new System.Drawing.Size(463, 257);
+            this.Login_area.TabIndex = 0;
+            this.Login_area.TabStop = false;
+            this.Login_area.Text = "Login Area";
+            this.Login_area.Enter += new System.EventHandler(this.Login_area_Enter);
+            // 
+            // button_cancel
+            // 
+            this.button_cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_cancel.Location = new System.Drawing.Point(144, 206);
+            this.button_cancel.Name = "button_cancel";
+            this.button_cancel.Size = new System.Drawing.Size(86, 33);
+            this.button_cancel.TabIndex = 11;
+            this.button_cancel.Text = "Cancel";
+            this.button_cancel.UseVisualStyleBackColor = true;
+            this.button_cancel.Click += new System.EventHandler(this.button_cancel_Click);
+            // 
+            // button_login
+            // 
+            this.button_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_login.Location = new System.Drawing.Point(364, 206);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(75, 33);
+            this.button_login.TabIndex = 10;
+            this.button_login.Text = "Login";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(269, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "level_error";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(184, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Level";
+            // 
+            // comboBox_level
+            // 
+            this.comboBox_level.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox_level.FormattingEnabled = true;
+            this.comboBox_level.Items.AddRange(new object[] {
+            "~Select One~"});
+            this.comboBox_level.Location = new System.Drawing.Point(272, 32);
+            this.comboBox_level.Name = "comboBox_level";
+            this.comboBox_level.Size = new System.Drawing.Size(177, 24);
+            this.comboBox_level.TabIndex = 7;
+            this.comboBox_level.SelectedIndexChanged += new System.EventHandler(this.comboBox_level_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(269, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "password_error";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_password.Location = new System.Drawing.Point(272, 150);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(177, 22);
+            this.textBox_password.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(184, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(269, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "username_error";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_username.Location = new System.Drawing.Point(272, 89);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(177, 22);
+            this.textBox_username.TabIndex = 2;
+            this.textBox_username.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(184, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(126, 117);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Login_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 309);
+            this.Controls.Add(this.Login_area);
+            this.Name = "Login_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Screen";
+            this.Login_area.ResumeLayout(false);
+            this.Login_area.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Login_area;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox_level;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Label label4;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button_cancel;
+        private System.Windows.Forms.Button button_login;
+    }
+}
+
---
+++ Shule Yenu/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shule_Yenu
+{
+    public partial class Login_Form : Form
+    {
+        public Login_Form()
+        {
+           
+            InitializeComponent();
+            comboBox_level.SelectedIndex = 0;
+            populate();
+
+
+        }
+
+        
+
+        private void Login_area_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_cancel_Click(object sender, EventArgs e)
+        {
+            DialogResult daDialogResult = MessageBox.Show("Are you sure you intend to cancel login?",
+                "Login Cancellation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (daDialogResult == DialogResult.No)
+            {
+                
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void comboBox_level_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void populate()
+        {
+            string connectiontring = @"data source=DESKTOP-06IKRA6\ANKO_CHIF;initial catalog=alloys_academy;integrated security=True;";
+            SqlConnection mySqlConnection = new SqlConnection(connectiontring);
+            mySqlConnection.Open();
+            string sql_query = "Select * from login_categories";
+            SqlCommand mySqlCommand = new SqlCommand(sql_query, mySqlConnection);
+            SqlDataReader myDataReader = null;
+            myDataReader = mySqlCommand.ExecuteReader();
+            ArrayList myArrayList = new ArrayList();
+            while (myDataReader.Read())
+            {
+                string k = myDataReader["category_name"].ToString();
+                myArrayList.Add(k);
+            }
+
+            foreach (string cat in myArrayList)
+            {
+                comboBox_level.Items.Add(cat);
+                //Console.WriteLine(cat);
+            }
+            mySqlConnection.Close();
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+    }
+
